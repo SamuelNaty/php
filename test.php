3cab538c93a4a32a3236a0b0dea218db8e4c1759
Samuel Naty <113522335+SamuelNaty@users.noreply.github.com>
2022-12-07 17:28:30 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXO 1</title>
+</head>
+    <body>
+<?php
+     echo'<font color="red">EXERCICE 1</font>';
+     $nombre = rand(0,100);     
+     if ($nombre%2 == 1){
+            echo'<div style="background-color:blue">Le nombre '.$nombre.'  est impaire </div>';
+        }
+        else {
+            echo'<div style="background-color:red">Le nombre '.$nombre.' est paire </div>';
+        }
+      
+?>
+<?php
+        
+         
+         $tableau = array(
+   
+            case1 => 1,
+            case2 => 2,
+            case3 => 3,
+            case4 => 4,
+            case5 => 5,
+
+         );
+          for ($i = 0; $i < sizeof($tableau); $i++)
+          {
+              echo"A l'indice $i le chiffre est $tableau[] <br>";
+          }  
+?>
+
+    </body>
+</html>
+
+
